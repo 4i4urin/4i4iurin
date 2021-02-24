087053fc2256c4f777b428b302288e4f782c6a86
4i4urin <56293411+4i4urin@users.noreply.github.com>
2021-02-24 17:12:57 +0300

    Update slkf.c

--- slkf.c
+++ slkf.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-
-#define N 20
+#include <math.h>
 
 int main(void)
 {
@@ -10,10 +9,11 @@ int main(void)
     printf(" i  |  volt_1  |    volt_2\n");
     do
     {
-        printf("%3d : %0.6f | %0.6f\n", i, volt_1, volt_2);
-        volt_1 = 0.04 - 0.04 * volt_2 - 0.86 * volt_1;
-        volt_2 = 0.024 + 0.976 * volt_2 - 0.004 * volt_1;
+    	volt_2_old = volt_2; volt_1_old = volt_1;
+        printf("%0.6f\n", volt_1_old);
+        volt_2 = 0.024 + 0.976 * volt_2_old - 0.004 * volt_1_old;
+        volt_1 = 0.04 - 0.04 * volt_2_old + 0.86 * volt_1_old;
         i++;
-    } while (i < N);
+    }  while ((fabs(volt_1 - volt_1_old) > 0.0001) && (fabs(volt_2 - volt_2_old) > 0.0001));
     return 0;
 }
