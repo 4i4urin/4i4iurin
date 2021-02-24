bbfb14c328119a207a2bd612d2527d3f528642db
4i4urin <56293411+4i4urin@users.noreply.github.com>
2021-02-24 17:15:05 +0300

    Update slkf.c

--- slkf.c
+++ slkf.c
@@ -10,7 +10,7 @@ int main(void)
     do
     {
     	volt_2_old = volt_2; volt_1_old = volt_1;
-        printf("%0.6f\n", volt_1_old);
+        printf("%3d : %0.6f | %0.6f\n", i, volt_1_old, volt_2_old);
         volt_2 = 0.024 + 0.976 * volt_2_old - 0.004 * volt_1_old;
         volt_1 = 0.04 - 0.04 * volt_2_old + 0.86 * volt_1_old;
         i++;
